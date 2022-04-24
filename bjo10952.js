0f6629c66efae38fd645e062c850fd61fa7dfd82
seunghoonKang <tmdgns4321@gmail.com>
2022-04-24 19:35:21 +0900

    A+B -5

---
+++ bjo10952.js
@@ -0,0 +1,19 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+//예제 입력
+// 1 1  -> 1+1 =2
+// 2 3 -> 2+3 = 5
+// 3 4 
+// 9 8
+// 5 2
+// 0 0
+
+
+input.forEach(element => {let result = element.split(' '); 
+  if(Number(result[0]) + Number(result[1]) == 0){
+    null;
+  }
+  else{
+  console.log(Number(result[0]) + Number(result[1])) }
+});
