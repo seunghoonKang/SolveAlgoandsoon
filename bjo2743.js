f31ea626e986948dfd5e3c8c32764d4ab45a9ad4
seunghoon <tmdgns4321@gmail.com>
2023-04-17 17:58:20 +0900

    백준 2743 단어 길이 재기

---
+++ bjo2743.js
@@ -0,0 +1,5 @@
+let fs = require("fs");
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+let input = fs.readFileSync("input.txt").toString().trim();
+
+console.log(input.length);
