c4967a3673a87ab616844eade2d55a48ddbac976
seunghoonKang <59612529+seunghoonKang@users.noreply.github.com>
2023-09-02 03:26:31 +0900

    [Silver IV] Title: ATM, Time: 200 ms, Memory: 11812 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.js
+++ 백준/Silver/11399. ATM/ATM.js
@@ -1,20 +1,14 @@
 const path = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const fs = require("fs");
-let [N, P] = fs.readFileSync(path).toString().trim().split(`\n`);
-
-const result = [];
+let [_, P] = fs.readFileSync(path).toString().trim().split(`\n`);
 
 P = P.split(" ")
   .map(Number)
   .sort((a, b) => a - b);
 
-for (let i = 0; i < N; i++) {
-  let temp = 0;
-  for (let j = 0; j <= i; j++) {
-    temp += P[j];
-  }
-  result.push(temp);
-}
+const increaseTime = P.map((_, index) =>
+  P.slice(0, index + 1).reduce((sum, curr) => sum + curr)
+);
 
-let res = result.reduce((sum, curr) => sum + curr);
+let res = increaseTime.reduce((sum, curr) => sum + curr);
 console.log(res);
