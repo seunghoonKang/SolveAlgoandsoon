dbb1cf7c5e336e3e653674bd5a3555076a209757
seunghoonKang <59612529+seunghoonKang@users.noreply.github.com>
2023-12-20 23:48:04 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 124 ms, Memory: 9292 KB -BaekjoonHub

--- 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.js
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.js
@@ -1,6 +1,2 @@
-const path = process.platform === "linux" ? "/dev/stdin" : "input.txt";
-const fs = require("fs");
-const n = fs.readFileSync(path).toString().trim();
-
 console.log(1);
 console.log(0);
